6b5ae3e6cc668775dec7c13ca6149e6b68d1de09
Paul MacLean <paul.maclean@rtraction.com>
2016-11-20 15:33:24 -0500

    added options to simple creation

--- test/map.test.js
+++ test/map.test.js
@@ -6,12 +6,34 @@ describe('Map', function() {
   });
 
   it('should create a polygon', function() {
-    var shape =  new google.maps.Polygon();
+    var triangleCoords = [
+      {lat: 25.774, lng: -80.190},
+      {lat: 18.466, lng: -66.118},
+      {lat: 32.321, lng: -64.757},
+      {lat: 25.774, lng: -80.190}
+    ];
+    var options = {
+      paths: triangleCoords,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35
+    };
+
+    var shape = new google.maps.Polygon(options);
     expect(shape).toBeDefined();
   });
 
   it('should create a marker', function() {
-    var marker =  new google.maps.Marker();
+    var map = new google.maps.Map();
+    var myLatLng = {lat: -25.363, lng: 131.044};
+    var options = {
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    };
+    var marker = new google.maps.Marker(options);
     expect(marker).toBeDefined();
   });
 
